7a3cd4fce9ac3fbcc9f1d7f27448d25c4915c7aa
pserotini <p.serotini@gmail.com>
2023-12-10 17:07:26 -0300

    functional model/API + successful tests

--- challenge/api.py
+++ challenge/api.py
@@ -1,6 +1,11 @@
 import fastapi
+# from model import DelayModel
+from challenge.model import DelayModel
+from fastapi import HTTPException  # Import HTTPException
+import pandas as pd
 
 app = fastapi.FastAPI()
+delay_model = DelayModel()  # Initialize the DelayModel when the application starts
 
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
@@ -9,5 +14,20 @@ async def get_health() -> dict:
     }
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(data: dict) -> dict:
+    delay_model.load("model_v1_0.mdl")  # Load the pre-trained model when the application starts
+
+    input_data = pd.DataFrame(data['flights'])  # Convert input data to DataFrame
+    
+    # Preprocess the input data
+    try:
+        features = delay_model.preprocess(input_data)
+    except Exception as preprocess_error:
+        raise HTTPException(status_code=400, detail=f"Preprocessing error: {str(preprocess_error)}")
+    
+    # Make predictions
+    try:
+        predictions = delay_model.predict(features)
+        return {"predictions": predictions}
+    except Exception as prediction_error:
+        raise HTTPException(status_code=400, detail=f"Prediction error: {str(prediction_error)}")
